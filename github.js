de39284839af118031e32fc33db5db5c3e8dac22
sasha_zhardetskaya <sasha310209@gmail.com>
2018-03-13 11:38:27 +0200

    add few error hanlers

--- github.js
+++ github.js
@@ -2,28 +2,39 @@ const https = require('https');
 
 function getRepos(username, done) {
 
+    if(!username) return done (new Error('please, write username'));
+
     const options = {
         hostname: 'api.github.com',
         path: `/users/${username}/repos`,
         headers: { 'User-Agent': 'SashaZhardetskaya' }
     };
 
-    https.get(options, res => {
+    const req = https.get(options, res => {
         // console.log(res.statusCode, res.statusMessage) //1
 
         res.setEncoding('utf-8');
 
         // res.on('data', data => console.log(data, data.length)) //2
 
-        let body = '';
+        if(res.statusCode === 200) {
+            let body = '';
 
-        res.on('data', data => body += data);
+            res.on('data', data => body += data);
 
-        res.on('end', () => {
-            const result = JSON.parse(body);
-            done(null, result)
-        })
-    })
+            res.on('end', () => {
+                const result = JSON.parse(body);
+                done(null, result)
+            })
+        } else {
+            done(new Error(`Couldn't receive data from server (${res.statusCode} ${res.statusMessage})`))
+        }
+
+
+    });
+
+    // req.on('error', error => done(error)); //3 Появ-ся если отправить неправильный запрос, например вместо options написать несущ-й url
+    req.on('error', error => done(new Error(`couldn't send request (${error.message})`)));
 }
 
 module.exports = {
